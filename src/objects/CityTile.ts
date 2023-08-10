86152993cf717c265d82279d2d703e7f751e492d
n-takeshita <naokisyuu@gmail.com>
2023-08-10 18:39:34 +0900

    add: CityTileの配置可能かの確認ロジックを実装

--- src/objects/CityTile.ts
+++ src/objects/CityTile.ts
@@ -1,4 +1,4 @@
-import { type CityTileColor } from 'enums'
+import { type CityTileColor, MapSpaceType } from 'enums'
 import { type CityTileState, context } from 'game'
 import { type MapSpace, type GoodsCube, type Line } from 'objects'
 
@@ -42,5 +42,23 @@ export class CityTile {
 
   public get linkedFixedLines (): Line[] {
     throw new Error('Not implemented')
+  }
+
+  /**
+   * 敷設が可能か
+   */
+  public canPlaceToMapSpace (mapSpace: MapSpace): boolean {
+    const { p } = context()
+
+    if (p === null) throw new Error('user is not in the game')
+    if (!p.hasTurn) throw new Error('user is not turn player')
+
+    // 町スペース以外
+    if (mapSpace.type !== MapSpaceType.TOWN) return false
+
+    // すでに都市タイルが引かれている
+    if (mapSpace.cityTile !== null) return false
+
+    return true
   }
 }
