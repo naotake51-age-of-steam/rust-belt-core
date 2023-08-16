723f42c0745a8729820aa1f627bd1980cbd3e66c
n-takeshita <naokisyuu@gmail.com>
2023-08-16 18:58:53 +0900

    add: HasDelayExecuteを定義（対象のフェーズに遷移したら一定時間後にexecuteDelayを実行してGameの状態を進める）

--- src/game/Phase/Phase.ts
+++ src/game/Phase/Phase.ts
@@ -1,7 +1,12 @@
 import { type PhaseId } from 'enums'
+import { type Game } from 'game/Game'
 
 export interface Phase {
   id: PhaseId
   message: string
   deepCopy: () => Phase
+}
+
+export interface HasDelayExecute {
+  executeDelay: () => Game
 }
